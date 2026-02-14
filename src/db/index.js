f24ecfc7c6efb12da37ad342235b68e2c3c6472b
Ritesh Kumar <riteshpeepal@gmail.com>
2026-02-14 16:12:18 +0530

    import  verifyJWT

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { exit } from 'node:process';
 const connectDB = async ()=> {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
+        console.log(`\n✅ MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
         
     } catch (error) {
         console.log('ERROR: MONGODB connection error',error)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import{ Router } from "express";
 import { loginUser, logoutUser, registerUser } from "../controlers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router()
 
 router.route("/register").post(
@@ -21,6 +22,6 @@ router.route("/login").post(loginUser)
 
 // Secured routes
 
-router.route("/logout").post(veryfyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
